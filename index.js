836d767106c8df1a377dc62c9298c459f000c34e
Qiawox <gv7361299@gmail.com>
2022-06-01 16:19:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -34,25 +34,36 @@ const todo = {
         const divik = document.createElement('div');
         divik.classList.add('divik');
         const spic = document.createElement('li');
-        spic.insertAdjacentHTML('beforeend', this.create1(elemDesc.value));
-        const elemNumber = document.querySelector('.number');
-        spic.insertAdjacentHTML('beforeend', this.create2(elemNumber.value));
-        const elemText = document.querySelector('.name');
-        divik.insertAdjacentHTML('afterbegin', this.create(elemText.value));
-        elemText.value = '';
-        divik.appendChild(spic);
-        document.querySelector('.result').appendChild(divik);
-        divik.insertAdjacentHTML('beforeend', this.create3());
+            spic.insertAdjacentHTML('beforeend', this.create1(elemDesc.value));
+            const elemNumber = document.querySelector('.number');
+            spic.insertAdjacentHTML('beforeend', this.create2(elemNumber.value));
+            const elemText = document.querySelector('.name');
+            divik.insertAdjacentHTML('afterbegin', this.create(elemText.value));
+            elemText.value = '';
+            divik.appendChild(spic);
+            document.querySelector('.result').appendChild(divik);
+            divik.insertAdjacentHTML('beforeend', this.create3());
+            const desc = document.querySelector('.rrr').value;
+            const number = document.querySelector('.number').value;
+            const object = {
+                desc: desc,
+                number: number
+            }
+            localStorage.setItem('value', JSON.stringify(object));
 
+            document.querySelector('.bl').style.display = 'none';
+            console.log('win2');
+            localStorage.setItem('todo', document.querySelector('.result').innerHTML);
+            document.querySelector('.bl').setAttribute('data-todo-state', 'off');
     },
     create(text) {
-        return `<span class="todo__task"><b>${text}</b></span><br>`;
+        return `<span class="todo__task"><b class="t1">${text}</b></span><br>`;
     },
     create1(desc) {
-        return `<span>${desc}`;
+        return `<span class="t2">${desc}`;
     },
     create2(number) {
-        return `<span> - ${number}h</span><br><br>`;
+        return `<span> - <p class="t3">${number}</p>h</span><br><br>`;
     },
     create3() {
         return `<button class="bts" onclick="click1(event)">Load</button>
